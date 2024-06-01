acbada89eabbfbae918d74d72de3fb1ae66eb0ee
Ashwin <hellloajr@gmail.com>
2024-06-01 11:08:28 +0530

    flexcode added

--- src/shell.java
+++ src/shell.java
@@ -1,7 +1,42 @@
 import java.util.Scanner;
+import py4j.GatewayServer;
 
 public class shell {
+
+    String text;
+    public void set_text(String text)
+    {
+        this.text = text;
+    }
+
+    public String get_result()
+    {
+        SymbolTable global_symbol_table = new SymbolTable();
+        Context context = new Context("<program>");
+        context.symbolTable = global_symbol_table;
+
+        Tokenizer l = new Tokenizer("<stdin>", text);
+            Tokenizer tokens = l.make_tokens();
+            if(tokens.error != null) return(tokens.error.repr());
+            else{
+                //System.out.println(tokens.repr());
+                Parser p = new Parser(tokens.toks);
+                ParseResult ast = p.parse();
+                if(ast.err != null) return(ast.err.repr());
+                else{
+                    //System.out.println(ast.node.toString());
+                    Interpreter interpreter = new Interpreter();
+                    RTResult result = interpreter.visit(ast.node, context);
+                    if(result.err != null) return(result.err.repr());
+                    else return(((Object)result.value).toString());
+                }
+            }
+    }
     public static void main(String[] args) {
+
+        GatewayServer g = new GatewayServer(new shell(), 25530);
+        g.start();
+        System.out.println("Gateway server started.");
 
         SymbolTable global_symbol_table = new SymbolTable();
         Context context = new Context("<program>");
